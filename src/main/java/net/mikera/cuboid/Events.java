1963f8f0d5a0c97e2c688290be1f6fcf795930f5
Mike <mike@mikera.net>
2021-07-03 12:50:14 +0800

    Misc edits

--- src/main/java/net/mikera/cuboid/Events.java
+++ src/main/java/net/mikera/cuboid/Events.java
@@ -7,6 +7,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.Material;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.event.block.Action;
 
 public class Events implements Listener {
@@ -20,18 +21,21 @@ public class Events implements Listener {
 		if (e.getClickedInventory().getHolder() instanceof SelectionScreen) {
 			e.setCancelled(true);
 			Player player = (Player) e.getWhoClicked();
-			if (e.getCurrentItem() == null) {
+			ItemStack item=e.getCurrentItem() ;
+			if (item == null) {
 				return;
 			}
-			if (e.getCurrentItem().getType() == Material.LIME_STAINED_GLASS_PANE) {
+			if (item.getType() == Material.LIME_STAINED_GLASS_PANE) {
 				player.sendMessage(ChatColor.GREEN + "You have selected Yes");
 				player.closeInventory();
 			} else if (e.getSlot() == 4) {
 				player.sendMessage(ChatColor.AQUA + "Please make a selection...");
-			} else if (e.getCurrentItem().getType() == Material.RED_STAINED_GLASS_PANE) {
+			} else if (item.getType() == Material.RED_STAINED_GLASS_PANE) {
 				player.sendMessage(ChatColor.RED + "You have selected No");
 				player.closeInventory();
 			}
+			
+			
 		}
 	}
 
